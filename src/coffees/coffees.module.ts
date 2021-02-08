e4f88aa67484c7a18c62c5ed5cadcbd4fe4b561d
NestorDevs <neivertjosias@gmail.com>
2021-02-08 18:27:27 -0400

    add typeOrm and postgres sql

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,14 @@
+import { Flavor } from './entities/flavor.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
 import { Module } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+import { Event } from 'src/events/entities/event.entity';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+})
 export class CoffeesModule {}
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,36 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
+
+@Entity() // sql table === 'coffee'
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ nullable: true })
   name: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column()
   brand: string;
-  flavors: string[];
+
+  @Column({ default: 0 })
+  recomendations: number;
+
+  @JoinTable()
+  @ManyToMany(
+    type => Flavor,
+    flavor => flavor.coffees,
+    {
+      cascade: true, // ['insert']
+    },
+  )
+  flavors: Flavor[];
 }
